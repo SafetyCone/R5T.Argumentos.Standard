c955742269e15c0f9601bfb29a6e7a99ea747ec2
MinexAutomation <minex.automation@gmail.com>
2020-05-02 19:03:54 -0700

    Added IServiceCollection extensions.

---
+++ source/R5T.Argumentos.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.Argumentos.Default;
+
+
+namespace R5T.Argumentos.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ICommandLineArgumentsProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddCommandLineArgumentsProvider(this IServiceCollection services)
+        {
+            services.AddDefaultCommandLineArgumentsProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ICommandLineArgumentsProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddCommandLineArgumentsProvider<TCommandLineArgumentsProvider>(this IServiceCollection services)
+            where TCommandLineArgumentsProvider: ICommandLineArgumentsProvider
+        {
+            services.AddCommandLineArgumentsProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ICommandLineArgumentsProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ICommandLineArgumentsProvider> AddCommandLineArgumentsProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICommandLineArgumentsProvider>(() => services.AddCommandLineArgumentsProvider());
+            return serviceAction;
+        }
+    }
+}
